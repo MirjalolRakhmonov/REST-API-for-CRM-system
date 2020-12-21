e4663c0d388975226742feae9483a1e83cd2adf0
Mirjalol Rakhmonov <manhattan701@gmail.com>
2020-12-21 10:18:59 +0100

    First step from Steps.txt file is performed

--- src/main/java/com/mirjalolcode/springdemo/service/CustomerService.java
+++ src/main/java/com/mirjalolcode/springdemo/service/CustomerService.java
@@ -1,8 +1,8 @@
-package com.luv2code.springdemo.service;
+package com.mirjalolcode.springdemo.service;
 
 import java.util.List;
 
-import com.luv2code.springdemo.entity.Customer;
+import com.mirjalolcode.springdemo.entity.Customer;
 
 public interface CustomerService {
 
