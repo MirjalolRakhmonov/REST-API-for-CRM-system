69e43fca5e9375d9e05d4e15394a74ab7dc11e37
Mirjalol Rakhmonov <manhattan701@gmail.com>
2020-12-21 12:26:50 +0100

    Step 2, Update REST service to throw exception if customer is not found

---
+++ src/main/java/com/mirjalolcode/springdemo/rest/CustomerRestExceptionHandler.java
@@ -0,0 +1,13 @@
+package com.mirjalolcode.springdemo.rest;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@ControllerAdvice
+public class CustomerRestExceptionHandler {
+
+	// add an exception handler for CustomerNotFoundException
+	
+	
+	// add another exception handler... to catch any exception (catch all)
+	
+}
